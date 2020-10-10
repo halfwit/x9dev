13cbf985bc6e76ebd525cadefb385b21b6674621
halfwit <michaelmisch1985@gmail.com>
2020-10-10 14:22:22 -0700

    Move back to pointers

--- mouse.c
+++ mouse.c
@@ -53,7 +53,7 @@ x9devMouseRead(int *x, int *y, int *b)
     int n;
 
     /* Magic numbers here are the size of a message from /dev/mouse and its offsets */
-    if((n = c9read(x9di.ctx, &x9di->mouse->tag, x9di->mouse.f, x9di->mouse->wroff, 1 + 4 * 12)) <= 0)
+    if((n = c9read(x9di.ctx, &x9di.mouse->tag, x9di.mouse.f, x9di.mouse->wroff, 1 + 4 * 12)) <= 0)
         return 0;
 
     if (n != 1 + 4 * 12)
@@ -64,9 +64,9 @@ x9devMouseRead(int *x, int *y, int *b)
         return 0;
     }
 
-    *x = atoi(x9di.mouse->rdbuf[1 + 0 * 12]) - screen->r.min.x;
-    *y = atoi(x9di.mouse->rdbuf[1 + 1 * 12]) - screen->r.min.y;
-    *b = atoi(x9di.mouse->rdbuf[1 + 2 * 12]);
+    *x = atoi(x9di.mouse->rdbuf + 1 + 0 * 12) - screen->r.min.x;
+    *y = atoi(x9di.mouse->rdbuf + 1 + 1 * 12) - screen->r.min.y;
+    *b = atoi(x9di.mouse->rdbuf + 1 + 2 * 12);
 
     return 1;
 }
