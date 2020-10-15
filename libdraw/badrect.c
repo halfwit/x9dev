11b83f7dd56472170a866245501a4f5c6cfd7112
halfwit <michaelmisch1985@gmail.com>
2020-10-15 16:36:38 -0700

    Add in badrect

---
+++ libdraw/badrect.c
@@ -0,0 +1,20 @@
+#include "draw.h"
+
+/*
+ * check for zero, negative size or insanely huge rectangle.
+ */
+int
+badrect(Rectangle r)
+{
+	int x, y;
+	unsigned int z;
+
+	x = Dx(r);
+	y = Dy(r);
+	if(x > 0 && y > 0){
+		z = x*y;
+		if(z/x == y && z < 0x10000000)
+			return 0;
+	}
+	return 1;
+}
